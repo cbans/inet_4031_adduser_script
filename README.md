f6e276c7a9995991be66ee1e0235315e3d9fb484
carter c <chen6681@umn.edu>
2025-11-07 20:05:53 +0000

    Final push attempt

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# inet_4031_adduser_script
+# INET4031 Add Users Script and User List
+
+Program Description
+
+create-users.py automates the creation of Linux user accounts and group memberships. 
+Instead of manually running commands like adduser and passwd for each new user, 
+this script reads a list of accounts from an input file and generates the required system commands automatically. 
+This helps reduce repetitive work, prevents typing errors, and scales easily for bulk user provisioning.
+
+How to Use the Program
+
+The script reads user account information from an input file and applies those settings to the system. Because it modifies system users and groups, it must be run with elevated (root) privileges.
+Input File Format (create-users.input)
+Each line in the file represents one user and must follow this colon-separated format:
+
+username – The Linux login name (ex: user04)
+password – The account’s initial password
+last_name/ first_name – Used to fill in the system’s user description field (GECOS)
+groups – A comma-separated list of supplemental groups (ex: group01,group02)
+Use - if the user should not be added to any extra groups
+
+Skipping Lines:
+To skip a line (for notes or temporary removal), start the line with #.
+
+Command Execution
+
+The script must have executable permission set (e.g., $ chmod +x create-users.py) and is run from the command line using input redirection with elevated privileges.
+The necessary execution command is: sudo ./create-users.py < create-users.input
+
+Dry Run
+
+A Dry Run is a crucial safety feature that allows the user to test the script's logic and ensure it generates the 
+correct commands without making any permanent modifications to the system. To enable a dry run, the user must:
+
+Comment out the three lines containing os.system(cmd).
+
+Uncomment the three lines containing print(cmd).
+
+When run in this mode, the script will execute all its internal logic but will only output the exact adduser and passwd commands 
+that would have been run, allowing the user to check for errors before committing the changes.
--- create-users.py
+++ create-users.py
@@ -1,71 +1,73 @@
 #!/usr/bin/python3
 
 # INET4031
-# Your Name
-# Data Created
-# Date Last Modified
+# Carter Chen
+# 11/6/2025
+# 11/7/2025
 
-#REPLACE THIS COMMENT - identify what each of these imports is for.
+# Import necessary modules for OS interaction, Regular Expressions, and Standard Input.
 import os
 import re
 import sys
 
-#YOUR CODE SHOULD HAVE NONE OF THE INSTRUCTORS COMMENTS REMAINING WHEN YOU ARE FINISHED
-#PLEASE REPLACE INSTRUCTOR "PROMPTS" WITH COMMENTS OF YOUR OWN
+
 
 def main():
     for line in sys.stdin:
 
-        #REPLACE THIS COMMENT - this "regular expression" is searching for the presence of a character - what is it and why?
-        #The important part is WHY it is looking for a particular characer - what is that character being used for?
-        match = re.match("^#",line)
+        #Iterate through each line read from Standard Input (stdin), which is piped
+        #from the create-users.input file via the '<' redirection operator.
+        match = re.match("^#",line) # This character is used as a skip mechanism to ignore commented lines in the input file.
 
-        #REPLACE THIS COMMENT - why is the code doing this?
+        # Remove leading/trailing whitespace and split the line into a list of fields
         fields = line.strip().split(':')
 
-        #REPLACE THESE COMMENTS with a single comment describing the logic of the IF 
-        #what would an appropriate comment be for describing what this IF statement is checking for?
-        #what happens if the IF statement evaluates to true?
-        #how does this IF statement rely on what happened in the prior two lines of code? The match and fields lines.
-        #the code clearly shows that the variables match and the length of fields is being checked for being != 5  so why is it doing that?
+        #This conditional statement checks for two exclusion conditions
+        #If 'match' is true (the line is commented out).
+        #If the line does not contain exactly 5 data fields.
+        #If either condition is true, the 'continue' keyword skips the rest of the loop
+        #and moves to the next line of input, preventing runtime errors.
         if match or len(fields) != 5:
             continue
 
-        #REPLACE THIS COMMENT - what is the purpose of the next three lines. How does it relate to what is stored in the passwd file?
+        #Map the colon-delimited fields from the input file to clear variable names.
         username = fields[0]
         password = fields[1]
         gecos = "%s %s,,," % (fields[3],fields[2])
 
-        #REPLACE THIS COMMENT - why is this split being done?
+        #This line splits that field into a list of individual group names.
         groups = fields[4].split(',')
 
-        #REPLACE THIS COMMENT - what is the point of this print statement?
+        #Print a status message to the console indicating the start of the account creation process
         print("==> Creating account for %s..." % (username))
-        #REPLACE THIS COMMENT - what is this line doing?  What will the variable "cmd" contain.
+        #Execute the generated Linux command to create the user account on the system.
         cmd = "/usr/sbin/adduser --disabled-password --gecos '%s' %s" % (gecos,username)
 
-        #REMOVE THIS COMMENT AFTER YOU UNDERSTAND WHAT TO DO - these statements are currently "commented out" as talked about in class
-        #The first time you run the code...what should you do here?  If uncommented - what will the os.system(cmd) statemetn attempt to do?
-        #print cmd
-        #os.system(cmd)
+        #Currently set for Dry Run: Prints the command
 
-        #REPLACE THIS COMMENT - what is the point of this print statement?
+        #print (cmd)
+	#Uncomment this line and comment the 'print' line for Live Run
+        os.system(cmd)
+
+        #Print a status message to the console indicating the start of the password setting process.
         print("==> Setting the password for %s..." % (username))
-        #REPLACE THIS COMMENT - what is this line doing?  What will the variable "cmd" contain. You'll need to lookup what these linux commands do.
+        #This command uses 'echo' to pipe the password (twice) into 'passwd' via 'sudo' to set the password without user interaction.
         cmd = "/bin/echo -ne '%s\n%s' | /usr/bin/sudo /usr/bin/passwd %s" % (password,password,username)
 
-        #REMOVE THIS COMMENT AFTER YOU UNDERSTAND WHAT TO DO - these statements are currently "commented out" as talked about in class
-        #The first time you run the code...what should you do here?  If uncommented - what will the os.system(cmd) statemetn attempt to do?
-        #print cmd
-        #os.system(cmd)
+        # Execute the generated Linux command to set the user's password.
+        # Currently set for Dry Run: Prints the command
+        #print (cmd)
+	# Uncomment this line and comment the 'print' line for Live Run
+        os.system(cmd)
 
         for group in groups:
-            #REPLACE THIS COMMENT with one that answers "What is this IF statement looking for and why? If group !='-' what happens?"
+            #Check if the group name is NOT a hyphen ('-'). The hyphen is used in the input file
+	    #to signify that the user should not be added to any supplemental groups.
             if group != '-':
                 print("==> Assigning %s to the %s group..." % (username,group))
                 cmd = "/usr/sbin/adduser %s %s" % (username,group)
-                #print cmd
-                #os.system(cmd)
+                #print (cmd)
+                os.system(cmd)
 
 if __name__ == '__main__':
     main()
